8708d9a8800376111a85e8e65005c6e1fc11b042
petitsurume <gamemannew2013@yahoo.co.jp>
2017-01-27 10:53:11 +0900

    [WIP]favorite implement

--- model/post.rb
+++ model/post.rb
@@ -25,5 +25,41 @@ module Plugin::Pnutio
         def perma_link
             Retriever::URI("https://pnut.io/@"+user.username+"/"+id)
         end
+
+        # ふぁぼ実装
+
+        def favorite(_fav=true)
+            Deferred.new {
+                if _fav then
+                    res = API::put_with_auth("posts/"+id+"/bookmark")["meta"]["code"] < 400
+                else
+                    res = API::delete_with_auth("posts/"+id+"/bookmark")["meta"]["code"] < 400
+                end
+                if res then
+                    youBookmarked=_fav
+                end
+                res
+            }
+        end
+
+        def favorite?
+            youBookmarked
+        end
+
+        def favorited_by
+            []
+        end
+
+        def favoritable?
+            true
+        end
+
+        def favorited_by_me?(me)
+            favorite?
+        end
+        
+        def introducer(me=Service.primary!)
+            self
+        end
     end
 end
