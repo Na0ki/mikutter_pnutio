5cb31327003ad97f28058c36d1a2d817af23eab7
petitsurume <gamemannew2013@yahoo.co.jp>
2017-01-27 16:25:23 +0900

    インデントを変えた

--- model/post.rb
+++ model/post.rb
@@ -1,107 +1,107 @@
 # -*- coding: utf-8 -*-
 
 module Plugin::Pnutio
-    class Post < Retriever::Model
-        include Retriever::Model::MessageMixin
+  class Post < Retriever::Model
+    include Retriever::Model::MessageMixin
 
-        register :pnutio_post, name: "pnut.io Post", timeline: true
+    register :pnutio_post, name: "pnut.io Post", timeline: true
 
-        field.time :created, required:true
-        field.string :id, required:true
-        field.string :text, required:true
-        field.string :source
-        field.has :user, Plugin::Pnutio::User, required:true
-        field.int :bookmarksCount
-        field.int :repostsCount
-        field.int :repliesCount
-        field.int :threadsCount
-        field.bool :youBookmarked
-        field.bool :youReposted
+    field.time :created, required:true
+    field.string :id, required:true
+    field.string :text, required:true
+    field.string :source
+    field.has :user, Plugin::Pnutio::User, required:true
+    field.int :bookmarksCount
+    field.int :repostsCount
+    field.int :repliesCount
+    field.int :threadsCount
+    field.bool :youBookmarked
+    field.bool :youReposted
 
-        def to_show
-            @to_show ||= self[:text]
-        end
-
-        def perma_link
-            Retriever::URI("https://pnut.io/@"+user.username+"/"+id)
-        end
-
-        # dictからpostを作る
-
-        def self.for_dict(dict)
-            post = Post.new(
-                created: Time.iso8601(dict["created_at"]),
-                id: dict["id"],
-                text: dict["content"]["text"] || "",
-                source: dict["source"]["name"]+"(with pnut.io)",
-                user: User.for_dict(dict["user"]),
-                bookmarksCount: dict["counts"]["bookmarks"],
-                repostsCount: dict["counts"]["reposts"],
-                repliesCount: dict["counts"]["replies"],
-                threadsCount: dict["counts"]["threads"],
-                youBookmarked: dict["you_bookmarked"],
-                youReposted: dict["you_reposted"]
-            )
-            post.created.localtime post.user.timezone_offset
-            post
-        end
-
-        # ふぁぼ実装
-
-        def favorite(_fav=true)
-            Deferred.new {
-                if _fav == true
-                    res = API::put_with_auth("posts/"+id+"/bookmark")
-                else
-                    res = API::delete_with_auth("posts/"+id+"/bookmark")
-                end
-                result = res["meta"]["code"] < 400
-                # エラーでももうやってあるっぽい場合はやったことにする
-                if result == false
-                    if _fav == false && res["meta"]["error_message"] == "Post not bookmarked."
-                        result = true
-                    elsif _fav == true and res["meta"]["error_message"] == "Bookmark already exists."
-                        result=true
-                    end
-                end
-                if result
-                    my_user = User.for_dict(UserConfig[:pnutio_user_object])
-                    if _fav
-                        Plugin.call :favorite, Service.primary, my_user, self
-                    else
-                        Plugin.call :unfavorite, Service.primary, my_user, self
-                    end
-                    self.youBookmarked=_fav
-                else
-                    p res["meta"]
-                    Deferred.fail(res["meta"]["error_message"])
-                end
-                result
-            }
-        end
-
-        def unfavorite
-            favorite(_fav:false)
-        end
-
-        def favorite?
-            youBookmarked
-        end
-
-        def favorited_by
-            []
-        end
-
-        def favoritable?
-            true
-        end
-
-        def favorited_by_me?(me)
-            favorite?
-        end
-        
-        def introducer(me=Service.primary!)
-            self
-        end
+    def to_show
+      @to_show ||= self[:text]
     end
+
+    def perma_link
+      Retriever::URI("https://pnut.io/@"+user.username+"/"+id)
+    end
+
+    # dictからpostを作る
+
+    def self.for_dict(dict)
+      post = Post.new(
+        created: Time.iso8601(dict["created_at"]),
+        id: dict["id"],
+        text: dict["content"]["text"] || "",
+        source: dict["source"]["name"]+"(with pnut.io)",
+        user: User.for_dict(dict["user"]),
+        bookmarksCount: dict["counts"]["bookmarks"],
+        repostsCount: dict["counts"]["reposts"],
+        repliesCount: dict["counts"]["replies"],
+        threadsCount: dict["counts"]["threads"],
+        youBookmarked: dict["you_bookmarked"],
+        youReposted: dict["you_reposted"]
+      )
+      post.created.localtime post.user.timezone_offset
+      post
+    end
+
+    # ふぁぼ実装
+
+    def favorite(_fav=true)
+      Deferred.new {
+        if _fav == true
+          res = API::put_with_auth("posts/"+id+"/bookmark")
+        else
+          res = API::delete_with_auth("posts/"+id+"/bookmark")
+        end
+        result = res["meta"]["code"] < 400
+        # エラーでももうやってあるっぽい場合はやったことにする
+        if result == false
+          if _fav == false && res["meta"]["error_message"] == "Post not bookmarked."
+            result = true
+          elsif _fav == true and res["meta"]["error_message"] == "Bookmark already exists."
+            result=true
+          end
+        end
+        if result
+          my_user = User.for_dict(UserConfig[:pnutio_user_object])
+          if _fav
+            Plugin.call :favorite, Service.primary, my_user, self
+          else
+            Plugin.call :unfavorite, Service.primary, my_user, self
+          end
+          self.youBookmarked=_fav
+        else
+          p res["meta"]
+          Deferred.fail(res["meta"]["error_message"])
+        end
+        result
+      }
+    end
+
+    def unfavorite
+      favorite(_fav:false)
+    end
+
+    def favorite?
+      youBookmarked
+    end
+
+    def favorited_by
+      []
+    end
+
+    def favoritable?
+      true
+    end
+
+    def favorited_by_me?(me)
+      favorite?
+    end
+    
+    def introducer(me=Service.primary!)
+      self
+    end
+  end
 end
--- model/user.rb
+++ model/user.rb
@@ -3,90 +3,90 @@
 require 'tzinfo'
 
 module Plugin::Pnutio
-    class User < Retriever::Model
-        include Retriever::Model::UserMixin
+  class User < Retriever::Model
+    include Retriever::Model::UserMixin
 
-        field.string :id, required:true
-        field.time :created
-        field.string :locale
-        field.string :timezone
-        field.string :type
-        field.string :username, required:true
-        field.string :idname, required:true
-        field.string :name
-        field.string :profile_text
-        field.string :avatar_image_link, required:true
-        field.int :avatar_image_height
-        field.int :avatar_image_width
-        field.bool :avatar_image_is_default
-        field.string :cover_image_link
-        field.int :cover_image_height
-        field.int :cover_image_width
-        field.bool :cover_image_is_default
-        field.int :bookmarksCount
-        field.int :clientsCount
-        field.int :followersCount
-        field.int :followingCount
-        field.int :postsCount
-        field.int :usersCount
-        field.string :verifiedDomain
-        field.string :verifiedLink
-        field.bool :follows_you
-        field.bool :you_blocked
-        field.bool :you_follow
-        field.bool :you_muted
-        field.bool :you_can_follow
+    field.string :id, required:true
+    field.time :created
+    field.string :locale
+    field.string :timezone
+    field.string :type
+    field.string :username, required:true
+    field.string :idname, required:true
+    field.string :name
+    field.string :profile_text
+    field.string :avatar_image_link, required:true
+    field.int :avatar_image_height
+    field.int :avatar_image_width
+    field.bool :avatar_image_is_default
+    field.string :cover_image_link
+    field.int :cover_image_height
+    field.int :cover_image_width
+    field.bool :cover_image_is_default
+    field.int :bookmarksCount
+    field.int :clientsCount
+    field.int :followersCount
+    field.int :followingCount
+    field.int :postsCount
+    field.int :usersCount
+    field.string :verifiedDomain
+    field.string :verifiedLink
+    field.bool :follows_you
+    field.bool :you_blocked
+    field.bool :you_follow
+    field.bool :you_muted
+    field.bool :you_can_follow
 
-        def perma_link
-            Retriever::URI("https://pnut.io/@"+user.username)
-        end
-
-        # TODO: mikutterがidnameをfieldじゃないと表示してくれない不具合が直ったら消す
-        #def idname
-        #    p username
-        #    username
-        #end
-
-        def profile_image_url
-            avatar_image_link
-        end
-
-        def timezone_offset
-            TZInfo::Timezone.get(self.timezone).current_period.utc_offset
-        end
-
-        def self.for_dict(dict)
-            User.new(
-                id: dict["id"],
-                created: dict["created_at"],
-                locale: dict["locale"],
-                timezone: dict["timezone"],
-                type: dict["type"],
-                username: dict["username"],
-                # TODO: mikutterがidnameをfieldじゃないと表示してくれない不具合が直ったら消す
-                idname: dict["username"],
-                name: dict["name"],
-                profile_text: dict["content"]["text"],
-                avatar_image_link: dict["content"]["avatar_image"]["link"],
-                avatar_image_height: dict["content"]["avatar_image"]["height"],
-                avatar_image_width: dict["content"]["avatar_image"]["width"],
-                avatar_image_is_default: dict["content"]["avatar_image"]["is_default"],
-                cover_image_link: dict["content"]["cover_image"]["link"],
-                cover_image_height: dict["content"]["cover_image"]["height"],
-                cover_image_width: dict["content"]["cover_image"]["width"],
-                cover_image_is_default: dict["content"]["cover_image"]["is_default"],
-                bookmarksCount: dict["counts"]["bookmarks"],
-                clientsCount: dict["counts"]["clients"],
-                followersCount: dict["counts"]["followers"],
-                followingCount: dict["counts"]["following"],
-                postsCount: dict["counts"]["posts"],
-                usersCount: dict["counts"]["users"],
-                follows_you: dict["follows_you"],
-                you_blocked: dict["you_blocked"],
-                you_follow: dict["you_follow"],
-                you_muted: dict["you_muted"],
-                you_can_follow: dict["you_can_follow"]
-            )
-        end
+    def perma_link
+      Retriever::URI("https://pnut.io/@"+user.username)
     end
+
+    # TODO: mikutterがidnameをfieldじゃないと表示してくれない不具合が直ったら消す
+    #def idname
+    #  p username
+    #  username
+    #end
+
+    def profile_image_url
+      avatar_image_link
+    end
+
+    def timezone_offset
+      TZInfo::Timezone.get(self.timezone).current_period.utc_offset
+    end
+
+    def self.for_dict(dict)
+      User.new(
+        id: dict["id"],
+        created: dict["created_at"],
+        locale: dict["locale"],
+        timezone: dict["timezone"],
+        type: dict["type"],
+        username: dict["username"],
+        # TODO: mikutterがidnameをfieldじゃないと表示してくれない不具合が直ったら消す
+        idname: dict["username"],
+        name: dict["name"],
+        profile_text: dict["content"]["text"],
+        avatar_image_link: dict["content"]["avatar_image"]["link"],
+        avatar_image_height: dict["content"]["avatar_image"]["height"],
+        avatar_image_width: dict["content"]["avatar_image"]["width"],
+        avatar_image_is_default: dict["content"]["avatar_image"]["is_default"],
+        cover_image_link: dict["content"]["cover_image"]["link"],
+        cover_image_height: dict["content"]["cover_image"]["height"],
+        cover_image_width: dict["content"]["cover_image"]["width"],
+        cover_image_is_default: dict["content"]["cover_image"]["is_default"],
+        bookmarksCount: dict["counts"]["bookmarks"],
+        clientsCount: dict["counts"]["clients"],
+        followersCount: dict["counts"]["followers"],
+        followingCount: dict["counts"]["following"],
+        postsCount: dict["counts"]["posts"],
+        usersCount: dict["counts"]["users"],
+        follows_you: dict["follows_you"],
+        you_blocked: dict["you_blocked"],
+        you_follow: dict["you_follow"],
+        you_muted: dict["you_muted"],
+        you_can_follow: dict["you_can_follow"]
+      )
+    end
+  end
 end
